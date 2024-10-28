bfa5797ad929ff6ab7e189b3ad1007edd547b927
mohamed essam <mohamedessam2003147@gmail.com>
2024-10-28 23:06:37 +0200

    pick image from gallery

--- lib/feature/add_product/presentation/views/widgets/image_field.dart
+++ lib/feature/add_product/presentation/views/widgets/image_field.dart
@@ -1,19 +1,50 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class ImageField extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class ImageField extends StatefulWidget {
   const ImageField({super.key});
 
   @override
+  State<ImageField> createState() => _ImageFieldState();
+}
+
+class _ImageFieldState extends State<ImageField> {
+  bool isLoading = false;
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey, width: 1),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: const Icon(
-        Icons.image_outlined,
-        size: 250,
+    return Skeletonizer(
+      enabled: isLoading,
+      child: GestureDetector(
+        onTap: () async {
+          try {
+            isLoading = true;
+            setState(() {});
+            final ImagePicker imagePicker = ImagePicker();
+            final XFile? file =
+                await imagePicker.pickImage(source: ImageSource.gallery);
+            setState(() {
+              isLoading = false;
+            });
+          } catch (error) {
+            isLoading = false;
+            setState(() {});
+            log("there was an error when picking image $error");
+          }
+        },
+        child: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey, width: 1),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: const Icon(
+            Icons.image_outlined,
+            size: 250,
+          ),
+        ),
       ),
     );
   }
